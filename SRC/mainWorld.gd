51bff1e861c78e198fc6d8e7d6d61ef352e89947
dickinsona19 <dickinsona19@yahoo.com>
2022-08-30 09:11:45 -0400

    HUD works correctly

--- SRC/mainWorld.gd
+++ SRC/mainWorld.gd
@@ -26,11 +26,15 @@ func game_over():
 
 	 $ScoreTimer.stop()
 	 $MobTimer.stop()
+	 $HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 
 func _on_MobTimer_timeout():
@@ -60,6 +64,7 @@ func _on_MobTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_StartTimer_timeout():
